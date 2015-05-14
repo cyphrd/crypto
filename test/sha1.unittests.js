fb2c0dcd04c4ba02947cce49885587357ceb9eef
Benjamin Hutchins <ben@hutchins.co>
2015-05-14 01:29:53 -0400

    Added FIPS verifications to sha1, sha256 and sha512.

--- test/sha1.unittests.js
+++ test/sha1.unittests.js
@@ -8,4 +8,38 @@ describe('sha1', function () {
 			'a9993e364706816aba3e25717850c26c9cd0d89d'
 		);
 	});
+
+	it('php [tests taken from php unit tests]', function () {
+		assert.equal(
+			crypto.sha1('').hex(),
+			'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+		);
+
+		assert.equal(
+			crypto.sha1('a').hex(),
+			'86f7e437faa5a7fce15d1ddcb9eaeaea377667b8'
+		);
+
+		assert.equal(
+			crypto.sha1('012345678901234567890123456789012345678901234567890123456789').hex(),
+			'f52e3c2732de7bea28f216d877d78dae1aa1ac6a'
+		);
+	});
+
+	it('FIPS-180 Vectors', function () {
+		assert.equal(
+			crypto.sha1('abc').hex(),
+			'a9993e364706816aba3e25717850c26c9cd0d89d'
+		);
+
+		assert.equal(
+			crypto.sha1('abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq').hex(),
+			'84983e441c3bd26ebaae4aa1f95129e5e54670f1'
+		);
+
+		assert.equal(
+			crypto.sha1(Array(1000000+1).join('a')).hex(),
+			'34aa973cd4c4daa4f61eeb2bdbad27316534016f'
+		);
+	});
 });
--- test/sha256.unittests.js
+++ test/sha256.unittests.js
@@ -4,8 +4,35 @@ var crypto = require('..');
 describe('sha256', function () {
 	it('truism', function () {
 		assert.equal(
+			crypto.sha256('').hex(),
+			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+		);
+
+		assert.equal(
+			crypto.sha256('a').hex(),
+			'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'
+		);
+
+		assert.equal(
+			crypto.sha256('012345678901234567890123456789012345678901234567890123456789').hex(),
+			'5e43c8704ac81f33d701c1ace046ba9f257062b4d17e78f3254cbf243177e4f2'
+		);
+	});
+
+	it('FIPS-180 Vectors', function () {
+		assert.equal(
 			crypto.sha256('abc').hex(),
 			'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad'
+		);
+
+		assert.equal(
+			crypto.sha256('abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq').hex(),
+			'248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1'
+		);
+
+		assert.equal(
+			crypto.sha256(Array(1000000+1).join('a')).hex(),
+			'cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0'
 		);
 	});
 });
